2f04fbf142851e4e00ab7a41c1b7e553b5678ad1
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-20 11:40:14 +0000

    Reterive tasks test added

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,10 +1,16 @@
-import logging
 from tasks_app.serializers import TaskSerializer
+from tasks_app.models import Task
 from rest_framework import generics
 
 
 class ListCreateTasksView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
+
+
+class RetrieveUpdateDestroyTasksView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
--- tests/unit/test_task_views.py
+++ tests/unit/test_task_views.py
@@ -1,24 +1,22 @@
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
-from tasks_app.models import Location
+from tasks_app.models import Location, Task
 from django.contrib.auth.models import User
+from typing import Callable
 
 
 @pytest.mark.django_db
-def test_create_task():
+def test_create_task(test_user: User) -> None:
     # Given
-    user = User.objects.create_user(
-        username="testuser", password="testpass123"
-    )
     location = Location.objects.create(name="London")
     client = APIClient()
-    client.force_authenticate(user=user)
+    client.force_authenticate(user=test_user)
     url = reverse(
         "tasks-list-create"
     )  # Update with the correct URL name for task creation
     data = {
-        "owner": user.id,
+        "owner": test_user.id,
         "title": "Test Task",
         "description": "Test Task description",
         "completed": False,
@@ -31,3 +29,31 @@ def test_create_task():
     # Then
     assert response.status_code == 201
     assert response.data["description"] == data["description"]
+
+
+@pytest.mark.django_db
+def test_retrieve_tasks(
+    test_user: User, create_task: Callable[[], Task]
+) -> None:
+    create_task(
+        owner=test_user,
+        title="Test Task 1",
+        description="Test Task 1",
+        completed=False,
+    )
+    create_task(
+        owner=test_user,
+        title="Test Task 2",
+        description="Test Task 2",
+        completed=False,
+    )
+
+    client = APIClient()
+    client.force_authenticate(user=test_user)
+    url = reverse(
+        "tasks-list-create"
+    )  # Update with the correct URL name for task listing
+    response = client.get(url, format="json")
+
+    assert response.status_code == 200
+    assert len(response.data) == 2
