99d6b9877cabf36a49633ee719822e573dfadc3a
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-21 16:13:28 +0000

    Determine background color func added to return weather colors

---
+++ common.py
@@ -0,0 +1,10 @@
+def determine_background_color(weather_data: dict) -> str:
+    main_weather = weather_data["weather"][0]["main"].lower()
+    if main_weather in ["snow", "rain"]:
+        return "blue"
+    elif main_weather in ["clear"]:
+        return "red"
+    elif main_weather in ["clouds"]:
+        return "yellow-orange"
+    else:
+        return "gray"  # default color
