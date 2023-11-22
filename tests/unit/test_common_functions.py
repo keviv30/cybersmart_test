45bcfd303d6560308404a712b15d9a237c2bc33b
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-22 23:27:00 +0000

    Added tests for the common  determin background color function

---
+++ tests/unit/test_common_functions.py
@@ -0,0 +1,22 @@
+# test_weather.py
+import pytest
+from common import determine_background_color
+
+
+@pytest.mark.parametrize(
+    "weather_data, expected_color",
+    [
+        ({"weather": [{"main": "Snow"}]}, "blue"),
+        ({"weather": [{"main": "Rain"}]}, "blue"),
+        ({"weather": [{"main": "Clear"}]}, "red"),
+        ({"weather": [{"main": "Clouds"}]}, "yellow-orange"),
+        (
+            {"weather": [{"main": "Mist"}]},
+            "gray",
+        ),  # Testing an undefined weather condition
+    ],
+)
+def test_determine_background_color(
+    weather_data: dict, expected_color: str
+) -> None:
+    assert determine_background_color(weather_data) == expected_color
