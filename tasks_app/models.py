f83a79ce375d3654aa7284cf8727605279403aad
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-22 23:26:15 +0000

    Add tostring methods on the models

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,3 +17,6 @@ class Task(models.Model):
     location = models.ForeignKey(
         Location, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
