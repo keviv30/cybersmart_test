ba7caf7535e9ae119af7d057913bc81560542ac8
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-24 13:04:35 +0000

    Updated cors to interact with backend

--- todo_root/settings.py
+++ todo_root/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     "rest_framework",
     "tasks_app",
     "weather_app",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # Add other origins as needed
+]
